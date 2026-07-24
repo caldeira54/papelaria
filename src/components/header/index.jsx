62576df1d9b9eb1c46e65f1a2784abf20b577eeb
caldeira54 <caldeiraa54@gmail.com>
2026-07-24 11:13:33 -0300

    Responsividade dos componentes de cabeçalho, banner e rodapé

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+import "./style.css";
 
 import logo from "../../assets/logo.png";
 
-import './style.css';
-
 export default function Header() {
-    return (
-        <header className="header">
-            <div className="header-top">
-                <img src={logo} alt="Papelmix" className="logo" />
+  const [menuAberto, setMenuAberto] = useState(false);
 
-                <nav className="menu">
-                    <a href="#">Início</a>
-                    <a href="#">Produtos</a>
-                    <a href="#">Material Escolar</a>
-                    <a href="#">Escritório</a>
-                    <a href="#">Presentes</a>
-                    <a href="#">Promoções</a>
-                    <a href="#">Contato</a>
-                </nav>
-            </div>
-        </header>
-    );
+  function alterarMenu() {
+    setMenuAberto(!menuAberto);
+  }
+
+  function fecharMenu() {
+    setMenuAberto(false);
+  }
+
+  return (
+    <header className="header">
+      <div className="header-container">
+        <a href="#" className="logo-container">
+          <img
+            src={logo}
+            alt="Logo Papelmix Papelaria e Presentes"
+            className="logo"
+          />
+        </a>
+
+        <button
+          className="botao-menu"
+          onClick={alterarMenu}
+          aria-label="Abrir ou fechar menu"
+          aria-expanded={menuAberto}
+        >
+          {menuAberto ? <FaTimes /> : <FaBars />}
+        </button>
+
+        <nav className={`menu ${menuAberto ? "menu-aberto" : ""}`}>
+          <a href="#" onClick={fecharMenu}>Início</a>
+          <a href="#produtos" onClick={fecharMenu}>Produtos</a>
+          <a href="#material-escolar" onClick={fecharMenu}>Material Escolar</a>
+          <a href="#escritorio" onClick={fecharMenu}>Escritório</a>
+          <a href="#presentes" onClick={fecharMenu}>Presentes</a>
+          <a href="#promocoes" onClick={fecharMenu}>Promoções</a>
+          <a href="#contato" onClick={fecharMenu}>Contato</a>
+        </nav>
+      </div>
+    </header>
+  );
 }
